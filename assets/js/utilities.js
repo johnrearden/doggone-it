b32b8b73a148c4a70c08d62ab4fe07accdc41813
johnrearden <isnocturnal@gmail.com>
2022-06-05 18:38:49 +0000

    Create utilities.js and add method to get difference between 2 angles

---
+++ assets/js/utilities.js
@@ -0,0 +1,21 @@
+/**
+ * Gets the difference between the two angles provided in radians.
+ * Adjusts for the discontinuity when the angles are on either side
+ * of the negative x-axis, i.e. when one is between -PI/2 and -PI 
+ * and the other is between PI/2 and PI.
+ * @param {Number} angleFrom in radians
+ * @param {Number} angleTo in radians
+ * @returns the difference between the parameters in radians
+ */
+const getAngularDifference = (angleFrom, angleTo) => {
+    let difference = angleTo - angleFrom;
+    if (difference > Math.PI) {
+        difference -= 2 * Math.PI;
+    }
+    if (difference < -Math.PI) {
+        difference += 2 * Math.PI;
+    }
+    return difference;
+}
+
+export {getAngularDifference}
