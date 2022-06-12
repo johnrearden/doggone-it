26930edeb8300ebcc4e75ffff1569f0ac16a7edf
John Rearden <reardenjohn@gmail.com>
2022-06-12 09:17:07 +0100

    Remove code to split herd into nearest neighbour groups, add tests for herd.js

--- assets/js/herd.js
+++ assets/js/herd.js
@@ -36,7 +36,6 @@ export class Herd {
             this.xArray.push(newSheep);
             this.yArray.push(newSheep);
         }
-        this.#sortArrays();
     }
 
     /**
@@ -49,132 +48,47 @@ export class Herd {
     update(dog) {
 
         this.removeDepartedSheep();
-        
 
-        this.#sortArrays();
+        this.sortPositionArrays();
 
-        let preferredDist = dog.barking ?
-            SHEEP_NEIGHBOURLY_DISTANCE / 4:
-            SHEEP_NEIGHBOURLY_DISTANCE;
-        
+        this.centerX = this.calculateAverageXPosition();
+        this.centerY = this.calculateAverageYPosition();
+
         for (let sheep of this.xArray) {
-            // Get nearest neighbour set for each sheep.
-            let nearestNeighbours = new Set();
-
-            // Check along the increasing x-axis
-            let ownIndex = this.xArray.indexOf(sheep);
-            if (ownIndex < this.xArray.length - 1) { // don't run off the array
-                let pointer = ownIndex + 1;
-                whileLoop:
-                while (this.#evaluateDistance(this.xArray[pointer], sheep, preferredDist)) {
-                    nearestNeighbours.add(this.xArray[pointer]);
-                    pointer++;
-                    if (pointer === this.xArray.length) {
-                        break whileLoop; // We have reached the end of the array
-                    }
-                }
-            }
-            // Check along the decreasing x-axis
-            if (ownIndex > 0) {
-                let pointer = ownIndex - 1;
-                whileLoop:
-                while (this.#evaluateDistance(this.xArray[pointer], sheep, preferredDist)) {
-                    nearestNeighbours.add(this.xArray[pointer]);
-                    pointer--;
-                    if (pointer < 0) {
-                        break whileLoop;
-                    }
-                }
-            }
-            // Check along the increasing y-axis
-            ownIndex = this.yArray.indexOf(this);
-            if (ownIndex < this.yArray.length - 1) {
-                let pointer = ownIndex + 1;
-                whileLoop:
-                while (this.#evaluateDistance(this.yArray[pointer], sheep, preferredDist)) {
-                    nearestNeighbours.add(this.yArray[pointer]);
-                    pointer++;
-                    if (pointer = this.yArray.length) {
-                        break whileLoop;
-                    }
-                }
-                
-            }
-            // Check along the decreasing y-axis
-            if (ownIndex > 0) {
-                let pointer = ownIndex - 1;
-                whileLoop:
-                while (this.#evaluateDistance(this.yArray[pointer], sheep, preferredDist)) {
-                    nearestNeighbours.add(this.yArray[pointer]);
-                    pointer--;
-                    if (pointer < 0) {
-                        break whileLoop;
-                    }
-                }
-                
-            }
-
-            // Remove the sheep itself from its own nearest neighbour set.
-            nearestNeighbours.delete(sheep);
-
-            // Sort the array by decreasing distance;
-            let neighbourArray = Array.from(nearestNeighbours);
-            
-            this.#sortNeighboursByDistance(sheep, neighbourArray);
-
-            // Update the sheep, passing the set of nearest neighbours and 
-            // a reference to the dog.
-            sheep.update(neighbourArray, dog);
-
-            
+            // Update the sheep, passing a reference to the dog.
+            sheep.update(this.centerX, this.centerY, dog);
         }
-        
     }
 
     removeDepartedSheep() {
         // If sheep has left the field, remove it from the game.
-        this.xArray.filter(sheep => !(sheep.yPos < 0));
-        this.yArray.filter(sheep => !(sheep.yPos < 0));
-        document.getElementById("text-output2").innerHTML = `#sheep === ${this.xArray.length}`;
+        this.xArray = this.xArray.filter(sheep => sheep.yPos > 0);
+        this.yArray = this.yArray.filter(sheep => !(sheep.yPos < 0));
+        //document.getElementById("text-output2").innerHTML = `#sheep === ${this.xArray.length}`;
     }
 
-    #sortArrays() {
+    calculateAverageXPosition() {
+        let total = 0;
+        for(let i = 0; i < this.xArray.length; i++) {
+            total += this.xArray[i].xPos;
+        }
+        return total / this.xArray.length;
+    }
+
+    calculateAverageYPosition() {
+        let total = 0;
+        for(let i = 0; i < this.yArray.length; i++) {
+            total += this.yArray[i].yPos;
+        }
+        return total / this.yArray.length;
+    }
+
+    sortPositionArrays() {
         this.xArray.sort((sheep1, sheep2) => {
             return sheep1.xPos - sheep2.xPos;
         });
         this.yArray.sort((sheep1, sheep2) => {
             return sheep1.yPos - sheep2.yPos;
         });
-    }
-
-    #sortNeighboursByDistance(sheep, neighbours) {
-        neighbours.sort((a, b) => {
-            let aDistSqX = Math.pow(a.xPos - sheep.xPos);
-            let aDistSqY = Math.pow(a.yPos - sheep.yPos);
-            let bDistSqX = Math.pow(b.xPos - sheep.xPos);
-            let bDistSqY = Math.pow(b.yPos - sheep.yPos);
-            return (aDistSqX + aDistSqY) - (bDistSqX + bDistSqY);
-        });
-    }
-
-    /**
-     * This private method compares the sum of the squares of the distances
-     * between 2 sheep on each axis, and the square of the preferred distance
-     * from the herd center. It returns true if sheep1 is close enough to sheep2
-     * for them to be near neighbours.
-     * 
-     * The square root of the distances is not calculated as this is a computationally
-     * expensive operation, and redundant anyway.
-     * 
-     * @param {Sheep} sheep1 
-     * @param {Sheep} sheep2 
-     * @returns true if sheep1 and sheep2 are close enough to be near neighbours
-     */
-    #evaluateDistance(sheep1, sheep2, preferredDistance) {
-        let xDist = sheep1.xPos - sheep2.xPos;
-        let yDist = sheep1.yPos - sheep2.yPos;
-        let distanceSquared = Math.pow(xDist, 2) + Math.pow(yDist, 2);
-        let preferredDistSq = Math.pow(preferredDistance, 2);
-        return (distanceSquared <= preferredDistSq);
     }
 }
---
+++ assets/js/tests/herd.test.js
@@ -0,0 +1,77 @@
+import {Sheep} from '../sheep.js';
+import {FIELD_HEIGHT, FIELD_WIDTH} from '../constants.js';
+import { Herd } from '../herd.js';
+
+describe("Test the herd class", () => {
+    describe("Test the removeDepartedSheep function", () => {
+        let herd;
+        beforeEach(() => {
+            herd = new Herd(10);
+            
+        });
+        it("should leave length of xArray and yArray unchanged if all sheep are inside game area", () => {
+            let xStartingLength = herd.xArray.length;
+            let yStartingLength = herd.yArray.length;
+            herd.removeDepartedSheep();
+            expect(herd.xArray.length).toBe(xStartingLength);
+            expect(herd.yArray.length).toBe(yStartingLength);
+        });
+        it("should reduce length of xArray by 1 if one sheep is outside the game-area", () => {
+            let startingLength = herd.xArray.length;
+            herd.xArray[0].yPos = -10;
+            herd.removeDepartedSheep();
+            expect(herd.xArray.length).toBe(startingLength - 1);
+        });
+        it("should reduce length of yArray by 1 if one sheep is outside the game-area", () => {
+            let startingLength = herd.yArray.length;
+            herd.yArray[0].yPos = -10;
+            herd.removeDepartedSheep();
+            expect(herd.yArray.length).toBe(startingLength - 1);
+        });
+    });
+
+    describe("Test the calculateAverageXPosition function", () => {
+        let herd;
+        beforeEach(() => {
+            herd = new Herd(3);
+        });
+        it("should return 2 for average xPosition of [1, 2, 3]", () => {
+            herd.xArray[0].xPos = 1;
+            herd.xArray[1].xPos = 2;
+            herd.xArray[2].xPos = 3;
+            expect(herd.calculateAverageXPosition()).toBe(2);
+        });
+    });
+
+    describe("Test the calculateAverageYPosition function", () => {
+        let herd;
+        beforeEach(() => {
+            herd = new Herd(3);
+        });
+        it("should return 2 for average yPosition of [1, 2, 3]", () => {
+            herd.yArray[0].yPos = 1;
+            herd.yArray[1].yPos = 2;
+            herd.yArray[2].yPos = 3;
+            expect(herd.calculateAverageYPosition()).toBe(2);
+        });
+    });
+
+    describe("Test the sortPositionArrays function", () => {
+        let herd;
+        beforeEach(() => {
+            herd = new Herd(10);
+        });
+        it("should sort the xArray by increasing value of x", () => {
+            herd.sortPositionArrays();
+            for (let i = 1; i < herd.xArray.length; i++) {
+                expect(herd.xArray[i].xPos).toBeGreaterThanOrEqual(herd.xArray[i - 1].xPos);
+            }
+        });
+        it("should sort the yArray by increasing value of y", () => {
+            herd.sortPositionArrays();
+            for (let i = 1; i < herd.yArray.length; i++) {
+                expect(herd.yArray[i].yPos).toBeGreaterThanOrEqual(herd.yArray[i - 1].yPos);
+            }
+        });
+    });
+});
