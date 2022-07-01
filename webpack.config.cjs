0dca2c2818752ae5f7485a6ec773b24d22c01d9e
John Rearden <reardenjohn@gmail.com>
2022-07-01 23:25:19 +0100

    Remove redundant folder 'assets' from project All references to paths that included 'assets' must be modified

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     mode: 'development',
-    entry: "./src/assets/js/index.js",
+    entry: "./src/js/index.js",
     output: {
         path: path.resolve(__dirname, "public"),
         filename: "bundle.js"
